a677e4cd0d85236eae7cf0b8a17b0d4fabc1854e
Dipankar Nalui (dnalui) <dnalui@cisco.com>
2024-02-26 13:28:38 +0530

    Created folders for each interview, rearranged the files and folders

--- Company_interviews/IBM/Second_Round/swap numbers and store in file.py
+++ Company_interviews/IBM/Second_Round/swap numbers and store in file.py
@@ -11,7 +11,7 @@ b=tmp
 l1="after swap"
 l2 = "a = " + str(a)
 l3 = "b = " + str(b)
-with open("store.txt","w") as f:
+with open("store.txt", "w") as f:
     f.write(l1+"\n")
     f.write(l2+"\n")
     f.write(l3+"\n")
