8e547eb1da21812b8f45cfec0e240f0a72ee1ba4
Dipankar Nalui <dipankarnalui@gmail.com>
2024-10-07 21:12:31 +0530

    New Practice daily

---
+++ Networking/tcp_3way_handshake.py
@@ -0,0 +1,3 @@
+#SYN: Client sends a SYN request to start a connection.
+#SYN-ACK: Server responds with a SYN-ACK to acknowledge and synchronize.
+#ACK: Client sends an ACK to confirm, establishing the connection.
---
+++ Networking/tcp_4way_handshake_termination.py
@@ -0,0 +1,6 @@
+#1. Host A → Host B: FIN flag set.
+#2. Host B → Host A: ACK flag set.
+#3. Host B → Host A: FIN flag set.
+#4. Host A → Host B: ACK flag set.
+
+
