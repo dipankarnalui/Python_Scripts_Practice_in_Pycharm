0e68f4d436c99e1ad10cb30bec0c01a25d41916a
Dipankar Nalui <dipankarnalui@gmail.com>
2025-08-14 17:35:21 +0530

    z_os.py

---
+++ Company_interviews/IBM Z OS Hardware Networking/1.py
@@ -0,0 +1,98 @@
+'''
+#file
+#records
+#emp_name, salary, address
+#parse into output file
+
+
+#type of the file
+#max size, min
+#file permission, read, write
+#total no. of lines
+#csv, json, txt, comma,
+#search with pattern -- find it ? key ?
+#duplicate
+
+
+#-----
+#login page
+#HTTPS, HTTP
+#respose
+#username -- alphanumeric
+#password -- hidden, complexity, size, mixed
+#enter submit, login
+#dhashbaod navigation
+
+
+#API
+#server
+#SSH
+#IP
+#grep -iE "error|fail" /var/tmp/
+
+#HTTPS SSL
+#service
+#systemctl status start
+#nmap
+#nstat
+
+#automation
+#UI - selenium
+#backend - bash/python
+#xpath - ID,
+<div>
+<p1>
+<p1>
+<tr>
+for
+
+switch tab.
+switch
+
+
+$1 $2
+./choice.sh federal
+
+x
+
+777
+111 111 111
+rwx rwx rwx
+100
+4
+101
+500
+
+How to check if a file/dir exists or not in shell script ?
+if [ -d ]
+
+if [ -e ]
+
+cron 5*** application
+
+export PATH=
+export
+
+python
+
+n=125
+output=125,151,152,251,215
+forward
+backward
+for
+
+document string
+doc string
+
+
+TCP state
+
+
+networking
+TCP, DNS, commands, telnet, vs SSH
+which command to check whether the services are running or not
+wireshark filter example
+HTTP vs HTTPS differences
+stale dns?
+
+
